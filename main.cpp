3fc813d1dd2dd0ca6343af4c792e7e61f5af1f82
AzuraBlaze <93054462+AzuraBlaze@users.noreply.github.com>
2024-01-09 11:14:54 -0600

    Tested SugarPP headers

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
 #include "Player.hpp"
-
-#include <unordered_set>
-#include <vector>
+#include "SugarPP/io.hpp"
 
 int main()
 {
-    std::vector<Player> player_vec = 
-    {
-        { "John", 3, 4 },
-        { "Mary", 5, 6 },
-        { "Joseph", 8, 7 },
-        { "John", 4, 5 }
-    };
-
-    std::unordered_set<Player> player_set;
-
-    for(auto player : player_vec)
-        player_set.insert(player);
-    
-    for(auto player : player_set)
-        std::cout << player << std::endl;
+    int num = SugarPP::input<int>("Enter a number: ");
+    SugarPP::print("You entered:", num);
 }

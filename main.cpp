343053546a5dc698b51970df6ceeae3d79366ff9
AzuraBlaze <93054462+AzuraBlaze@users.noreply.github.com>
2024-01-09 11:30:18 -0600

    Tested all SugarPP headers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include "Player.hpp"
-#include "SugarPP/io.hpp"
+#include "SugarPP/all.hpp"
+
+#include <vector>
+
+using namespace std;
+using namespace SugarPP;
 
 int main()
 {
-    int num = SugarPP::input<int>("Enter a number: ");
-    SugarPP::print("You entered:", num);
+    cout << endl;
+
+    int num = input<int>("Enter a number: ");
+    print("You entered:", num, '\n');
+
+    vector<int> nums;
+
+    for(int n : Range(1, num + 1))
+        nums.push_back(n);
+
+    for(auto [i, value] : Enumerate(nums))
+        cout << '[' << to_string(i) << "] => " << value << endl;
+
+    cout << endl;
 }

0053738ffaae880e8ee111c871c24a71e6137f51
AzuraBlaze <93054462+AzuraBlaze@users.noreply.github.com>
2024-01-10 13:29:51 -0600

    Updated to test basic player generation and sorting

--- main.cpp
+++ main.cpp
@@ -3,24 +3,65 @@
 #include "SugarPP/all.hpp"
 
 #include <vector>
+#include <array>
+#include <span>
+#include <algorithm>
 
 using namespace std;
 using namespace SugarPP;
+
+static const vector<const string> names
+{
+    "QuantumQuest", "MysticJester", "LunaPhoenix", "CipherSeeker", "NebulaWhisper",
+    "ZenPioneer", "VelvetVortex", "EnigmaExplorer", "AstralHarmony", "ThunderMyst",
+    "StarlightGlider", "QuantumBard", "NeonNomad", "CelestialCipher", "SapphireEcho",
+    "ChronoWanderer", "SereneLabyrinth", "MiragePioneer", "TechnoSculptor", "PhoenixFable",
+    "SolarEcho", "ElysianVagrant", "ZephyrOracle", "PixelSorcerer", "NebulaLoom",
+    "QuantumQuasar", "EtherealCrafter", "VelvetSpectra", "PrismNomad", "CosmicPulse",
+    "LunarMystique", "StellarRogue", "EnchantVoyager", "PinnaclePeregrine", "RogueSpecter",
+    "NebulaNomad", "ZenithLoom", "EchoCipher", "AstralSculptor", "CelestialPioneer",
+    "QuantumWanderer", "NovaHarmony", "ChronoSorcerer", "SeraphicVoyager", "EonExplorer",
+    "MirageNomad", "TechnoSculptor", "RadiantPulse", "EtherealJester", "StellarWhisper"
+};
+
+vector<Player> generate_players()
+{
+    Range score_rng(0, 26);
+    vector<Player> players;
+    players.reserve(names.size());
+
+    for(const string& name : names)
+        players.emplace_back(name, score_rng.rand(), score_rng.rand());
+    
+    return players;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const span<T>& values)
+{
+    for(const T& value : values)
+        os << value << endl;
+    
+    return os;
+}
 
 int main()
 {
     cout << endl;
 
-    int num = input<int>("Enter a number: ");
-    print("You entered:", num, '\n');
+    vector<Player> players = generate_players();
 
-    vector<int> nums;
+    sort(players.begin(), players.end());
 
-    for(int n : Range(1, num + 1))
-        nums.push_back(n);
+    span<Player> players_span(players.begin(), players.end());
+    size_t chunk_size = players_span.size() / 3;
+    size_t leftover_size = players_span.size() % 3;
 
-    for(auto [i, value] : Enumerate(nums))
-        cout << '[' << to_string(i) << "] => " << value << endl;
+    span<Player> low_players(players_span.begin(), players_span.begin() + chunk_size);
+    span<Player> mid_players(low_players.end(), low_players.end() + chunk_size + leftover_size);
+    span<Player> high_players(mid_players.end(), players_span.end());
 
-    cout << endl;
+    cout << "\n[[ Low Players ]]\n\n" << low_players << endl;
+    cout << "\n[[ Mid Players ]]\n\n" << mid_players << endl;
+    cout << "\n[[ High Players ]]\n\n" << high_players << endl;
 }
